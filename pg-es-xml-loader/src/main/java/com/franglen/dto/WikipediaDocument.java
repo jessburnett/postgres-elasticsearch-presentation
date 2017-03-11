381fd233a213409bc5d674804eec4f667a7564be
Matthew Franglen <matthew@franglen.org>
2017-03-11 22:36:07 +0000

    Make wikipedia document immutable

--- pg-es-xml-loader/src/main/java/com/franglen/dto/WikipediaDocument.java
+++ pg-es-xml-loader/src/main/java/com/franglen/dto/WikipediaDocument.java
@@ -1,12 +1,14 @@
 package com.franglen.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
+@Builder(builderClassName = "Builder")
 @Data
 public class WikipediaDocument {
 
-    private String id;
-    private String title;
-    private String body;
+    private final String id;
+    private final String title;
+    private final String body;
 
 }
