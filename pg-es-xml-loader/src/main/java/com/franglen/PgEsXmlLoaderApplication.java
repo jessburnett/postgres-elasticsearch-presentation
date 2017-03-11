4c7d7385920a0248abf1a734595e0dc9739da5d9
Matthew Franglen <matthew@franglen.org>
2017-03-11 22:39:24 +0000

    Take first argument as file to read

--- pg-es-xml-loader/src/main/java/com/franglen/PgEsXmlLoaderApplication.java
+++ pg-es-xml-loader/src/main/java/com/franglen/PgEsXmlLoaderApplication.java
@@ -1,13 +1,26 @@
 package com.franglen;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.franglen.service.DocumentReader;
+
 @SpringBootApplication
-public class PgEsXmlLoaderApplication {
+public class PgEsXmlLoaderApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(PgEsXmlLoaderApplication.class, args);
+    }
+
+    private final DocumentReader reader;
+
+    public PgEsXmlLoaderApplication(DocumentReader reader) {
+        this.reader = reader;
+    }
+
+    public void run(String... args) throws Exception {
+        reader.read(args[0]);
     }
 
 }
