8d083ba498f263d7a806852b00fe307439e34329
Matthew Franglen <matthew@franglen.org>
2017-03-11 22:41:37 +0000

    Load spring integration configuration
    It's broken!

--- pg-es-xml-loader/src/main/java/com/franglen/PgEsXmlLoaderConfiguration.java
+++ pg-es-xml-loader/src/main/java/com/franglen/PgEsXmlLoaderConfiguration.java
@@ -4,8 +4,10 @@ import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
+@ImportResource("classpath:/integration.xml")
 public class PgEsXmlLoaderConfiguration {
 
     @Bean
