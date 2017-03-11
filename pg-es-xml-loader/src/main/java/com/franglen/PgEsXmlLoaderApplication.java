1d08e08eb9bbf91d3343f3c33b504fdb5586272c
Matthew Franglen <matthew@franglen.org>
2017-03-11 19:17:51 +0000

    Add start.spring.io generated java loader base
    The python loader hangs after some time. Not sure why. It should be easy
    to create a java loader that can write to both es and pg.

---
+++ pg-es-xml-loader/src/main/java/com/franglen/PgEsXmlLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.franglen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgEsXmlLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgEsXmlLoaderApplication.class, args);
+    }
+
+}
---
+++ pg-es-xml-loader/src/test/java/com/franglen/PgEsXmlLoaderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.franglen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PgEsXmlLoaderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
